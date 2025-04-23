57745df42c917df7987be09c15e73baa1ddc3634
nijoe1 <nikolaos@gitcoin.co>
2025-04-23 20:55:50 +0300

    feat: adds user applications tab  into the main page

--- src/app/grants/page.tsx
+++ src/app/grants/page.tsx
@@ -4,7 +4,7 @@ import WalletConnectButton from '@/components/auth/ConnectButton';
 import GrantsList from '@/components/common/GrantList';
 import { GrantCardSkeleton } from '@/components/common/skeletons/GrantCardSkeleton';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import {
   Select,
@@ -23,10 +23,44 @@ const Grants = () => {
   const { displayedGrants, loadMore, grants, isLoading, isFetched } =
     useGrants();
   const [searchTerm, setSearchTerm] = useState('');
+  const [showUserGrants, setShowUserGrants] = useState(true);
   const [filter, setFilter] = useState<FilterOption>(FilterOption.Highest);
 
   const { isFetched: isFetchedConnector } = useConnectorClient();
-  const { isConnected } = useAccount();
+  const { isConnected, address } = useAccount();
+
+  const filteredUserGrants = useMemo(() => {
+    let filtered = grants.filter((grant) => grant.address === address);
+
+    if (searchTerm) {
+      filtered = filtered.filter((grant) =>
+        grant.title.toLowerCase().includes(searchTerm.toLowerCase()),
+      );
+    }
+
+    switch (filter) {
+      case FilterOption.Highest:
+        filtered.sort(
+          (a, b) =>
+            Number(b.campaign.totalAmount) - Number(a.campaign.totalAmount),
+        );
+        break;
+      case FilterOption.Lowest:
+        filtered.sort(
+          (a, b) =>
+            Number(a.campaign.totalAmount) - Number(b.campaign.totalAmount),
+        );
+        break;
+      case FilterOption.MostClaimed:
+        filtered.sort((a, b) => b.claimed - a.claimed);
+        break;
+      case FilterOption.LeastClaimed:
+        filtered.sort((a, b) => a.claimed - b.claimed);
+        break;
+    }
+
+    return filtered;
+  }, [grants, address, searchTerm, filter]);
 
   const filteredAndSortedGrants = useMemo(() => {
     let filtered = displayedGrants;
@@ -79,60 +113,142 @@ const Grants = () => {
 
   return (
     <>
-      <div className="flex flex-col gap-6 items-start">
-        <h1 className="text-4xl font-bold">Grants</h1>
-        <p>{FEATURES.INTRO_TEXT}</p>
-      </div>
-      <div className="flex items-center gap-2 my-6">
-        <div className="relative w-full">
-          <Input
-            type="text"
-            placeholder="Search grant"
-            className="w-full pl-10 pr-4 py-2"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
-        </div>
-        <Select
-          value={filter}
-          onValueChange={(value) => setFilter(value as FilterOption)}
+      <div className="flex gap-10 items-center pb-6">
+        <Button
+          className={` rounded-none group relative bg-transparent text-black flex items-center font-semibold text-4xl px-0 ${
+            showUserGrants
+              ? 'border-b-4 border-secondary-foreground'
+              : 'before:absolute before:bottom-0 before:left-0 before:w-full before:h-1 before:bg-secondary-foreground before:transform before:scale-x-0 before:origin-right hover:before:scale-x-100 hover:before:origin-left before:transition-transform before:duration-300 before:ease-out'
+          }`}
+          variant={null}
+          onClick={() => setShowUserGrants(!showUserGrants)}
         >
-          <SelectTrigger className="w-[180px] bg-neutral-200">
-            <SelectValue placeholder="Select filter" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="Highest">Highest grant</SelectItem>
-            <SelectItem value="Lowest">Lowest grant</SelectItem>
-            <SelectItem value="MostClaimed">Most claimed</SelectItem>
-            <SelectItem value="LeastClaimed">Least claimed</SelectItem>
-          </SelectContent>
-        </Select>
+          My Applications
+        </Button>
+        <Button
+          className={`rounded-none group relative bg-transparent text-black flex items-center font-semibold text-4xl px-0 ${
+            !showUserGrants
+              ? 'border-b-4 border-secondary-foreground'
+              : 'before:absolute before:bottom-0 before:left-0 before:w-full before:h-1 before:bg-secondary-foreground before:transform before:scale-x-0 before:origin-right hover:before:scale-x-100 hover:before:origin-left before:transition-transform before:duration-300 before:ease-out'
+          }`}
+          variant={null}
+          onClick={() => setShowUserGrants(!showUserGrants)}
+        >
+          All Grants
+        </Button>
       </div>
-      <div className="bg-white w-full flex items-center justify-between py-3 px-10 rounded-lg mb-4">
-        <p className="font-semibold">
-          {isLoading ? 'Loading' : grants.length} Projects
-        </p>
-        <p className="font-semibold">
-          <span>Claimed</span> /{' '}
-          <span className="text-gray-500">Grant amount</span>
-        </p>
-      </div>
-      {isLoading ? (
-        <div className="space-y-4">
-          <GrantCardSkeleton />
-          <GrantCardSkeleton />
-          <GrantCardSkeleton />
-        </div>
-      ) : (
-        <GrantsList grants={filteredAndSortedGrants} />
+      {showUserGrants && (
+        <>
+          <div className="flex items-center gap-2 my-6">
+            <div className="relative w-full">
+              <Input
+                type="text"
+                placeholder="Search grant"
+                className="w-full pl-10 pr-4 py-2"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+              <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
+            </div>
+            <Select
+              value={filter}
+              onValueChange={(value) => setFilter(value as FilterOption)}
+            >
+              <SelectTrigger className="w-[180px] bg-neutral-200">
+                <SelectValue placeholder="Select filter" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="Highest">Highest grant</SelectItem>
+                <SelectItem value="Lowest">Lowest grant</SelectItem>
+                <SelectItem value="MostClaimed">Most claimed</SelectItem>
+                <SelectItem value="LeastClaimed">Least claimed</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="bg-white w-full flex items-center justify-between py-3 px-10 rounded-lg mb-4">
+            <p className="font-semibold">
+              {isLoading ? 'Loading' : filteredUserGrants.length} Projects
+            </p>
+            <p className="font-semibold">
+              <span>Claimed</span> /{' '}
+              <span className="text-gray-500">Grant amount</span>
+            </p>
+          </div>
+          {isLoading ? (
+            <div className="space-y-4">
+              <GrantCardSkeleton />
+              <GrantCardSkeleton />
+              <GrantCardSkeleton />
+            </div>
+          ) : (
+            <GrantsList grants={filteredUserGrants} />
+          )}
+          {filteredUserGrants.length < grants.length && (
+            <div className="mt-4 flex justify-center">
+              <Button onClick={loadMore} variant="outline">
+                Load More
+              </Button>
+            </div>
+          )}
+        </>
       )}
-      {filteredAndSortedGrants.length < grants.length && (
-        <div className="mt-4 flex justify-center">
-          <Button onClick={loadMore} variant="outline">
-            Load More
-          </Button>
-        </div>
+      {!showUserGrants && (
+        <>
+          <div className="flex flex-col gap-6 items-start">
+            <p>{FEATURES.INTRO_TEXT}</p>
+          </div>
+          <div className="flex items-center gap-2 my-6">
+            <div className="relative w-full">
+              <Input
+                type="text"
+                placeholder="Search grant"
+                className="w-full pl-10 pr-4 py-2"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+              <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
+            </div>
+            <Select
+              value={filter}
+              onValueChange={(value) => setFilter(value as FilterOption)}
+            >
+              <SelectTrigger className="w-[180px] bg-neutral-200">
+                <SelectValue placeholder="Select filter" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="Highest">Highest grant</SelectItem>
+                <SelectItem value="Lowest">Lowest grant</SelectItem>
+                <SelectItem value="MostClaimed">Most claimed</SelectItem>
+                <SelectItem value="LeastClaimed">Least claimed</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="bg-white w-full flex items-center justify-between py-3 px-10 rounded-lg mb-4">
+            <p className="font-semibold">
+              {isLoading ? 'Loading' : grants.length} Projects
+            </p>
+            <p className="font-semibold">
+              <span>Claimed</span> /{' '}
+              <span className="text-gray-500">Grant amount</span>
+            </p>
+          </div>
+          {isLoading ? (
+            <div className="space-y-4">
+              <GrantCardSkeleton />
+              <GrantCardSkeleton />
+              <GrantCardSkeleton />
+            </div>
+          ) : (
+            <GrantsList grants={filteredAndSortedGrants} />
+          )}
+          {filteredAndSortedGrants.length < grants.length && (
+            <div className="mt-4 flex justify-center">
+              <Button onClick={loadMore} variant="outline">
+                Load More
+              </Button>
+            </div>
+          )}
+        </>
       )}
     </>
   );
